5c7809ec9a369fc9f148ad0e4c7666fe9dd24400
SergioMadness <madness.sergio@gmail.com>
2015-12-08 00:35:03 +0300

    Gitlab models

---
+++ models/GitlabRepository.go
@@ -0,0 +1,12 @@
+package models;
+
+type GitlabRepository struct {
+	Id string `json:"id"`;
+	Message string `json:"message"`;
+	Timestamp string `json:"timestamp"`;
+	Url string `json:"url"`;
+	Added []string `json:"added"`;
+	Modified []string `json:"modified"`;
+	Removed []string `json:"removed"`;
+	Author GitlabChangeAuthor `json:"author"`;
+}
