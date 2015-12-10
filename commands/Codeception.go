aab5d4c21cec662e1635dc79945adac4c82b55dd
Sergey Zinchenko <s.zinchenko@rassvet.ru>
2015-12-10 16:14:09 +0300

    Command is added

---
+++ commands/Codeception.go
@@ -0,0 +1,57 @@
+package commands
+
+import (
+	"fmt"
+	"gitdeployer/helpers"
+	"os"
+)
+
+func Codecept(path string) (string, error) {
+	var out string
+	var err error
+
+	fmt.Println("Codeception tests")
+
+	currentDir, _ := os.Getwd()
+	os.Chdir(path + "/tests")
+
+	commandExists, _ := helpers.IsCommandExists("codecept")
+
+	if !commandExists {
+		DownloadCodecept(path)
+		out, err = CodeceptPHP(path)
+	} else if out, err = helpers.Exec("codecept", "build"); err == nil {
+		out, err = helpers.Exec("codecept", "run", "--steps")
+	}
+	os.Chdir(currentDir)
+	fmt.Println(out)
+	fmt.Println(err)
+
+	return out, err
+}
+
+func CodeceptPHP(path string) (string, error) {
+	var output string
+	var err error
+
+	currentDir, _ := os.Getwd()
+	os.Chdir(path)
+
+	if output, err = helpers.Exec("php", "codecept.phar", "build"); err == nil {
+		output, err = helpers.Exec("php", "codecept.phar", "run", "--steps")
+	}
+
+	os.Chdir(currentDir)
+
+	return output, err
+}
+
+func DownloadCodecept(path string) error {
+	fmt.Println("Codeception download")
+
+	err := helpers.DownloadFile("http://codeception.com/codecept.phar", path+"codecept.phar")
+	fmt.Println(err)
+	fmt.Println("Codeception downloaded")
+
+	return err
+}
